54c563374894f371a1c3ece535db7a68d1aa0da8
Yahnnosh <48091563+Yahnnosh@users.noreply.github.com>
2021-11-03 18:38:07 +0100

    Update dealer.py
    forgot to change debugging settings - fixed now

--- Limited action space/dealer.py
+++ Limited action space/dealer.py
@@ -41,7 +41,7 @@ class dealer:
         self.PENETRATION = 0.8
         self.VALUES = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8,
                        '9': 9, '10': 10, 'J': 10, 'Q': 10, 'K': 10, 'A': 1}
-        self.deck = ['A', '10']* (100) #['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'] * (4 * self.N_DECKS)
+        self.deck = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'] * (4 * self.N_DECKS)
         self.shuffle()
 
     def shuffle(self):
