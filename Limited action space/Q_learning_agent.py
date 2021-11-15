fc56f52f750ab27aa1f2ee106ebb69f4fc7e2b65
andreasr27 <andreasr27@hotmail.com>
2021-11-15 20:25:42 +0100

    Corrected a bug in the update rule of the 's' action (self.S was not updating at all)

--- Limited action space/Q_learning_agent.py
+++ Limited action space/Q_learning_agent.py
@@ -38,27 +38,31 @@ class QAgent(agent):
         actions = episode['actions']
         agent_hands = episode['hands']
         reward = episode['reward']
-
+        dealer_card = episode['dealer'][0][0]
+        # TODO: it is possible that `agent_hands` contains exactly one hand, while `actions` is an empty list. This is
+        # incorrect. Indeed, in that case `actions` should contain an 's'.
+        if not actions:
+            return
         if reward > 0:
             final_state_index = 0
         elif reward < 0:
             final_state_index = 1
         elif reward == 0:
             final_state_index = 2
-        dealer_card = episode['dealer'][0][0]
-        current_agent_hand = agent_hands.pop(0)
-        while agent_hands:
+        while len(agent_hands) > 1:
+            current_agent_hand = agent_hands.pop(0)
             # compute the next state
             next_agent_hand = agent_hands[0]
-
             # compute current's and next's state actions
             current_state_index = self.state_approx([current_agent_hand, dealer_card])
             next_state_index = self.state_approx([next_agent_hand, dealer_card])
-            if next_state_index >= self.NUMBER_OF_STATES or len(agent_hands) == 1:
-                next_state_index = final_state_index
             # compute the current action
             action = actions.pop(0)
-
+            # If the `next_state_index` is more than ---. then we lost
+            if next_state_index >= self.NUMBER_OF_STATES:
+                next_state_index = final_state_index
+                if not (final_state_index == 1):
+                    print("Houston we have a problem!")
             if action == 'h':
                 self.H[current_state_index] += self.alpha * (
                         reward + self.gamma * max(self.H[next_state_index], self.S[next_state_index]) - self.H[
@@ -68,6 +72,21 @@ class QAgent(agent):
                         reward + self.gamma * max(self.H[next_state_index], self.S[next_state_index]) - self.S[
                     current_state_index])
 
-            # prepare for the next iteration
-            #self.alpha = 1.0 / ((1.0 / self.alpha) + 0.2)
+        # In this case either actions is empty, which happens iff the last action was a 'h' and we got busted or actions
+        # contains exactly one element which is an 's'. We treat each case differently. In the former case we do nothing
+        # since the hand that has remained in the `agents_hand` does not correspond to a valid state, but just to a
+        # losing state (and we dealt with it inside the while loop). In tha latter case we need to update self.S
+        if actions:
+            # sanity check
+            if not (len(actions) == 1):
+                print("problem 1")
+            action = actions.pop(0)
+            if not (action == 's'):
+                print("problem 2")
+            # now the computation is repeated
             current_agent_hand = agent_hands.pop(0)
+            current_state_index = self.state_approx([current_agent_hand, dealer_card])
+            next_state_index = final_state_index
+            self.S[current_state_index] += self.alpha * (
+                reward + self.gamma * max(self.H[next_state_index], self.S[next_state_index]) - self.S[current_state_index])
+
