775e61ccaf2abf77b242fc3ae60a1b29075a10f6
kopylovvasily <93799558+kopylovvasily@users.noreply.github.com>
2021-12-14 00:57:24 +0100

    Agent update
    support alternative state approximation

--- Limited action space/agent.py
+++ Limited action space/agent.py
@@ -64,3 +64,23 @@ class agent(metaclass=ABCMeta):
         dealer_hand = hand[1]
         x, y, z = self.evaluate(agent_hand), self.evaluate([dealer_hand]), self.soft(agent_hand)
         return int((3 + (x - 4) * 20) + ((y - 2) * 2) + z)
+
+    def alternative_state_approx(self, hand): # state_approx + the number of cards in your hand
+        """
+        Approximates state to single number (index of state_approx vector
+        (starting from 0!)) - warning: does not assign to terminal states! -
+        --------------------------------------------------------------------
+        state_approx vector = [win, loss, draw,
+        [4, 2, 0], [4, 2, 1], [4, 3, 0], [4, 3, 1], ... ,
+        [4, 11, 0], [4, 11, 1], ..., [21, 11, 0], [21, 11, 1]]^T
+        where [x, y, a, z] = [sum of values of agent's hand, value of dealer's hand, number of cards in
+        the habd, bool(agent hand soft)]
+        :param hand: hand = [[card1, card2, ..., cardN], card_dealer, number of cards in the hand]
+        :return index of approximated state in state_approx vector (starting from 0!)
+        """
+        agent_hand = hand[0]
+        dealer_hand = hand[1]
+        x, y, z = self.evaluate(agent_hand), self.evaluate([dealer_hand]), self.soft(agent_hand)
+        return int((3 + (x - 4) * 20) + ((y - 2) * 2) + z)
+
+
