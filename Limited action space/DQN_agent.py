93ac5a5fb30631410001d104fb8be83ffe20fb0e
kopylovvasily <93799558+kopylovvasily@users.noreply.github.com>
2021-12-15 19:34:49 +0100

    Update of DQN

--- Limited action space/DQN_agent.py
+++ Limited action space/DQN_agent.py
@@ -19,20 +19,32 @@ import time
 # stand / hit / split / doubling 
 
 
-#FloatTensor = torch.FloatTensor
-#LongTensor = torch.LongTensor
-#ByteTensor = torch.ByteTensor
+import numpy as np
+import random
+#from BlackjackSM import BlackjackSM
+import pdb
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.autograd import Variable
+import time
+
+FloatTensor = torch.FloatTensor
+LongTensor = torch.LongTensor
+ByteTensor = torch.ByteTensor
 
 # hyperparameters
-BATCH_SIZE = 256
+BATCH_SIZE = 256 # TODO: change to 256 after debugging 
 GAMMA = 0.999
-#EPS_START = 1
+# EPS_START = 1
 EPS_START = 0.05
 EPS_END = 0.05
 EPS_DECAY = 20000
 WEIGHT_DECAY = 0.0001
 
-NUM_LAYERS = 3
+NUM_LAYERS = 11
 # k = np.rint(NUM_LAYERS / 2 + 0.5)
 k = 13
 num_episodes = 800000
@@ -46,11 +58,8 @@ C = 4
 graph_interval = 10000
 
 #state_machine = BlackjackSM()
-
-# NN has 4 inputs: (value of agent cards, soft/not, number of agent cards, value of a dealer card) 
-# and it has 2 outputs: Q(stand) and Q(hit)
-n_in = 4  
-n_out = 2 
+n_in = 4
+n_out = 2
 
 # build network
 layers_size = {-1: n_in}
@@ -67,8 +76,6 @@ for i in layers_size.keys():
 		modules.append(nn.BatchNorm1d(layers_size[i]))
 		modules.append(nn.ReLU())
 		# modules.append(nn.Dropout(0.15))
-
-# print(modules) # our NN 
 
 
 
@@ -106,7 +113,9 @@ class DQN(nn.Module):
 		return x
 
 # initialize model
-#model = DQN(modules)
+#modell = DQN(modules)
+#print(modell)
+#time.sleep(10)
 
 #try: 
 #	model.load_state_dict(torch.load("models/blackjack_DQN_" + str(NUM_LAYERS) + "-" + str(k) + ".pt"))
@@ -129,25 +138,54 @@ class DQN(nn.Module):
 class DQNAgent(agent):
 
 	def __init__(self):
-		#self.model = DQN(modules) # instantiation of the neural network
+		self.model = DQN(modules) # neural network with Stanford architecture 
 		self.memory = Memory(20000) # memory buffer 
 		self.gamma = 1
-		self.learning_rate = 1e-3
+		#self.learning_rate = 1e-2
+		#self.training = True
+		self.counter = 0
+		self.optimizer = optim.SGD(self.model.parameters(), lr=learning_rate, momentum=0.9, weight_decay=WEIGHT_DECAY)
+		self.training = True 
 
 		# basic NN, next we will perform optimization of its parameters 
+		'''
 		self.model = (torch.nn.Sequential(
-			torch.nn.Linear(5, 10), # first layer takes 5 values: 4 for the state and 1 for the action 
+			torch.nn.Linear(4, 8), # first layer takes 5 values: 4 for the state and 1 for the action 
 			torch.nn.ReLU(),
-			torch.nn.Linear(10, 32),
+			torch.nn.Linear(8, 16),
 			torch.nn.ReLU(),
-			torch.nn.Linear(32, 1),
+			torch.nn.Linear(16, 32),
+			torch.nn.ReLU(),
+			torch.nn.Linear(32, 16),
+			torch.nn.ReLU(),
+			torch.nn.Linear(16, 1),
 		))
+		'''
+		#self.model = (torch.nn.Sequential(
+		#	torch.nn.Linear(4, 52), # first layer takes 5 values: 4 for the state and 1 for the action 
+		#	torch.nn.ReLU(),
+		#	torch.nn.Linear(52, 38),
+		#	torch.nn.ReLU(),
+		#	torch.nn.Linear(27, 20),
+		#	torch.nn.ReLU(),
+		#	torch.nn.Linear(14, 10),
+		#	torch.nn.ReLU(),
+		#	torch.nn.Linear(10, 7),
+		#	torch.nn.ReLU(),
+		#	torch.nn.Linear(7, 5),
+		#	torch.nn.ReLU(),
+		#	torch.nn.Linear(5, 4),
+		#	torch.nn.ReLU(),
+		#	torch.nn.Linear(4, 3),
+		#	torch.nn.ReLU(),
+		#	torch.nn.Linear(3, 2),
+		#))
 
-	def state_approx(self, hand, action): # approximates state action pair as a tuple with 5 elements 
-		if action == 's':
-			action = 0
-		elif action == 'h':
-			action = 1
+	def state_approx(self, hand): # approximates the state as a tuple of 4 elements 
+		#if action == 's':
+		#	action = 0
+		#elif action == 'h':
+		#	action = 1
 
 		agent_hand = hand[0]
 		dealer_hand = hand[1]
@@ -155,10 +193,14 @@ class DQNAgent(agent):
 		agent_value = self.evaluate(agent_hand)
 		dealer_value = self.evaluate([dealer_hand])
 		agent_soft = self.soft(agent_hand)
-		number_of_cards = len(hand[0])  
+		number_of_cards = len(agent_hand)  
+		if number_of_cards == 2:
+			first_hand = 1 # the first hand 
+		else:
+			first_hand = 0 # not first hand 
 
-		# the state given in the tensor form  
-		state = torch.tensor([agent_value, dealer_value, agent_soft, number_of_cards, action], dtype=torch.float)
+		# the state is given in the tensor form  
+		state = torch.tensor([agent_value, dealer_value, agent_soft, first_hand], dtype=torch.float)
 		return state 
 
 
@@ -168,71 +210,151 @@ class DQNAgent(agent):
 	#	return y_pred
 	
 	def policy(self, hand): # returns next action for the current hand 
-
-		state_approx_stand = self.state_approx(hand, 's') # state action pair for stand 
-		state_approx_hit = self.state_approx(hand, 'h') # state action pair for hit 
-		
-		# forward pass to get Q values 
-		Q_pred_stand = self.model(state_approx_stand) 
-		Q_pred_hit = self.model(state_approx_hit) 
-		
+		state_approx = self.state_approx(hand) 
+		state_approx = state_approx.view(1,-1)
+		self.model.eval()
+		Q_stand, Q_hit = self.model(state_approx)[0].detach()
+	
 		# greedily return the action
-		if Q_pred_stand > Q_pred_hit:
+		if Q_stand > Q_hit:
 			action = 's'
 		else:
 			action = 'h'
+		
 
+		if self.training:
+			prob = random.uniform(0,1)
+			if prob > 0.8:
+				return random.choice(['h', 's'])
+			else:
+				return action
+		
 		return action
 
-	def Q_max(self, next_state): # len(next_state) = 4
-		state_approx_stand = self.state_approx(next_state, 's') 
-		state_approx_hit = self.state_approx(next_state, 'h')
-		Q_pred_stand = self.model(state_approx_stand) # forward pass
-		Q_pred_hit = self.model(state_approx_hit) # forward pass
-		Q_max = max([Q_pred_stand, Q_pred_hit])
-		return Q_max
 
-	def DQN_update(self, Q_true, state, action):
-		#Q_true = torch.tensor(Q_true).detach()
-		Q_true = torch.tensor([Q_true]).detach()
 
+
+	def optimize(self): # NN updates using a batch of experiences from the memory buffer 
+		if len(self.memory.memory) < BATCH_SIZE:
+			return # not enough experiences to generate the batch 	
+		self.model.train() # this tells Pytorch that we are in the training regime 
+		sample = self.memory.sample(BATCH_SIZE) # sample a batch of experiences from the buffer
+		batch = list(zip(*sample))
+			
+		state_batch = torch.stack(batch[0]) 
+		action_batch = batch[1] # actions of the batch
+		reward_batch = torch.tensor(batch[2]) # rewards of the batch 
+		next_state_batch = batch[3]
 	
+		new_action_batch = []
+		action_values = {'s': 0, 'h': 1}
+		for action in action_batch:
+			new_action_batch.append([action_values[action]]) # here actions are encoded with values 
+		new_action_batch = torch.tensor(new_action_batch) # convert to tensor 
 
-		state_appox = self.state_approx(state, action)
-		Q_pred = self.model(state_appox) # forward pass of the given state 
-		criterion = nn.SmoothL1Loss()
-		loss = criterion(Q_pred, Q_true) # compute Huber loss 
-		optimizer = torch.optim.RMSprop(self.model.parameters(), lr=self.learning_rate)
-		optimizer.zero_grad()
-		loss.backward()
-		optimizer.step()
+		Q_sa = self.model(state_batch) # compute NN estimates of Q values for the batch for all actions
+		Q_sa = Q_sa.gather(1, new_action_batch) # Q values for all state action pairs from the batch 
+
+		mask_next_state_batch = torch.tensor([0 if v is None else 1 for v in next_state_batch])
+		to_nn_next_state_batch = [v if v is not None else torch.tensor([0,0,0,0]) for v in next_state_batch]
+		to_nn_next_state_batch = torch.stack(to_nn_next_state_batch).float()
+		V_s = self.model(to_nn_next_state_batch)
+		V_s, _ = torch.max(V_s,dim=1)
+		V_s = V_s * mask_next_state_batch
+		observed_sa = reward_batch + self.gamma*V_s
+		observed_sa = observed_sa.view(-1,1).detach() # convert to the required size and disable gradient
+
+		loss = F.smooth_l1_loss(Q_sa, observed_sa) # compute hubber loss 
+		self.optimizer.zero_grad() # otherwise, gradient would be a combination of previously and newly gradients 
+		loss.backward() # calculate all gradients with respect to the weights  
+
+		# gradient clipping helps to mitigate exploding gradients issue 
+		nn.utils.clip_grad_norm(self.model.parameters(), max_norm=2, norm_type=2) 
+		
+		self.optimizer.step() # updates weights of NN
+		self.model.eval() # batch normalization layers behave differently for testing and training
 
 
 	def learn(self, episode): # NN learns after each episode 
 		actions = episode['actions']
 		agent_hands = episode['hands']
-		reward = episode['reward']
+		#reward = episode['reward']
 		dealer_card = episode['dealer'][0][0]
 
 		if not actions: 
 			return
+
 		if len(actions) != len(agent_hands): 
-			del agent_hands[-1] 
-       
+			del agent_hands[-1] # do not learn from busted hand 
+
 		while agent_hands: 
 			current_agent_hand = agent_hands.pop(0) # current hand 
-			next_agent_hand = agent_hands[0] if agent_hands else None # None if terminal 
+			next_agent_hand = agent_hands[0] if agent_hands else None # next hand, none if terminal 
 
-			current_state = [current_agent_hand, dealer_card]
+			current_state = [current_agent_hand, dealer_card] 
 			next_state = [next_agent_hand, dealer_card] if agent_hands else None
 
+			current_state = self.state_approx(current_state) 
+			next_state = self.state_approx(next_state) if agent_hands else None
+
 			if agent_hands:
-				Q_true = self.gamma * self.Q_max(next_state) # this is label if next state is not the terminal 
+				reward = 0
 			else:
-				Q_true = reward # this is label if the next state is the terminal   
+				reward = episode['reward'] # only if next state is the terminal
 
 			action = actions.pop(0)
-			self.DQN_update(Q_true, current_state, action) # updates NN  
+
+			experience = (current_state, action, reward, next_state) 
+			self.memory.push(experience) # stores the experince in the memory buffer
+
+			if (self.counter % 4) == 0: # every fourth step
+				self.optimize() 
+
+
+		# TODO: implement the policy method (epsilon greedy)
+		# TODO: initialization of the NN? 
+
+
+
+
+
+
+
+			
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+			#print('start')
+			#print(current_agent_hand)
+			#print(next_agent_hand)
+
+			#if agent_hands:
+			#	Q_true = self.gamma * self.Q_max(next_state) # this is label if next state is not the terminal 
+			#	Q_true = Q_true.detach()
+			#else:
+			#	Q_true = reward # this is label if the next state is the terminal   
+
+			#action = actions.pop(0)
+
+			#print(Q_true)
+			#print(action)
+			#time.sleep(10)
+			#self.DQN_update(Q_true, current_state, action) # updates NN  
 
 
 
@@ -248,9 +370,11 @@ class DQNAgent(agent):
 #test = agent.policy(hand)
 #print(test)
 
-'''
+
 
 # training helper functions
+
+'''
 counter = 0
 def select_action():
 	global counter
@@ -362,3 +486,4 @@ plt.xlabel("number of episodes")
 plt.plot(iterations, avg_reward_graph)
 plt.show()
 '''
+
--- Limited action space/evaluate_performance.py
+++ Limited action space/evaluate_performance.py
@@ -15,15 +15,18 @@ from card_counting import count_agent       # optimal baseline
 from value_iteration import value_iteration
 from fast_value_iteration import fast_value_iteration
 from Q_learning_agent import QAgent
+from double_q import double_QAgent
 from sarsa_agent import sarsa_agent
 from mc_agent import mc_agent
+from DQN_agent import DQNAgent
 
 from dealer import dealer
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 import sys
 import numpy as np
-
+import statistics
+import pandas as pd
 
 def simulate(policy, rounds, plot=False):
     """
@@ -41,14 +44,16 @@ def simulate(policy, rounds, plot=False):
 
     # params
     casino = dealer()
-    bank_account = [1000]   # starting money
+    bank_account = [1000000]   # starting money
     n_wins = 0
+    n_losses = 0
     game_over = False
     total_rewards = []
+    savings = 0
 
     # simulate
     # (tqdm shows progress bar)
-    for j in tqdm(range(rounds), leave=False, desc=str(type(policy))[8:].split('.')[0], file=sys.stdout, disable=False):
+    for j in tqdm(range(rounds), leave=False, file=sys.stdout, disable=True):
         # bet: static or dynamic
         bet = policy.bet if isinstance(policy, count_agent) else 1  # TODO: change this for add. dyn. policy
 
@@ -59,7 +64,7 @@ def simulate(policy, rounds, plot=False):
             game_over = True
 
         # play one round
-        episode = casino.play_round(policy, bet=bet, learning=False)    # testing independent from training
+        episode = casino.play_round(policy, bet=bet, learning=False) # testing independent from training
         reward = episode['reward']
 
         # update params
@@ -72,10 +77,13 @@ def simulate(policy, rounds, plot=False):
         if reward > 0:
             n_wins += 1
 
+        if reward < 0:
+            n_losses += 1
+
         total_rewards.append(reward)
 
     # calculate performance params
-    mean_win_rate = round(n_wins / rounds, 3)
+    mean_win_rate = round(n_wins / (n_wins+n_losses), 4)
     long_term_profitability = bank_account[-1]
     mean_loss_per_round = round(sum(total_rewards) / rounds, 3)
     std_loss_per_round = round(np.std(np.array(total_rewards)), 3)
@@ -87,27 +95,31 @@ def simulate(policy, rounds, plot=False):
             bank_account.append(curr_bank_account) # in case lost all their money
         plt.plot([j for j in range(rounds + 1)], bank_account, label=str(type(policy))[8:].split('.')[0])
 
-    return mean_win_rate, long_term_profitability, mean_loss_per_round, std_loss_per_round
+    return mean_win_rate, long_term_profitability, mean_loss_per_round, std_loss_per_round 
 
 
 if __name__ == '__main__':
     # Select policies
     policies = [
-        random_agent(),
-        dealer_policy(),
-        table_agent(),
-        count_agent(),
-        mc_agent(),
-        sarsa_agent(),
-        QAgent(alpha=0.01)
-        #fast_value_iteration()
+        #double_QAgent(),
+        #random_agent(),
+        #dealer_policy(),
+        #table_agent(),
+        #count_agent(),
+        #mc_agent(),
+        #sarsa_agent(),
+        #QAgent()
+        DQNAgent()
         ]
+    
     policy_names = [str(type(policy))[8:].split('.')[0] for policy in policies]
 
     # Select rounds
     training_rounds = 100000
     testing_rounds = 100000
 
+    #agent = table_agent()
+    
     # Training phase
     print('Starting training')
     _RETURN_NONE = (lambda: None).__code__.co_code
@@ -116,7 +128,7 @@ if __name__ == '__main__':
         if policy.learn.__code__.co_code != _RETURN_NONE:
             casino = dealer()
             # agent has implemented learn
-            for t in range(training_rounds):
+            for t in tqdm(range(training_rounds), leave=False, file=sys.stdout, disable=True):
                 casino.play_round(policy, bet=1, learning=True) # train agent
             print('Finished training for', policy_names[i])
             # sarsa needs explicit call
@@ -125,6 +137,32 @@ if __name__ == '__main__':
         else:
             # agent has not implemented learn
             pass
+
+    '''
+    loss_per_round = []
+
+    print("testing")
+    for i, policy in enumerate(policies):
+        for _ in range(10):
+            mean_win_rate, long_term_profitability, mean_loss_per_round, std_loss_per_round = simulate(policy, testing_rounds, plot=False)
+            loss_per_round.append(mean_loss_per_round)
+            print(mean_loss_per_round)
+        print("calculations")
+        mean = statistics.mean(loss_per_round)
+        stdev = statistics.stdev(loss_per_round)
+        print(mean)
+        print(stdev)
+        loss_per_round = []
+        
+        df_H = pd.DataFrame(policy.H_visitations) 
+        df_S = pd.DataFrame(policy.S_visitations)                
+
+        df_H.to_csv('H_visitations.csv') 
+        df_S.to_csv('S_visitations.csv') 
+
+    '''  
+
+    policies[0].training = False
 
     # Testing phase
     print('\nStarting testing')
@@ -146,4 +184,5 @@ if __name__ == '__main__':
     plt.legend(loc='upper right')
     plt.xlabel('rounds')
     plt.ylabel('bank account')
-    plt.show()
+    # plt.show()
+  
