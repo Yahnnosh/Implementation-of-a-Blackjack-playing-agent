f64be20f952af1fb0b94aaf86b83534027297d73
kopylovvasily <93799558+kopylovvasily@users.noreply.github.com>
2021-12-15 20:22:05 +0100

    code cleaning

--- Limited action space/DQN_agent.py
+++ Limited action space/DQN_agent.py
@@ -1,8 +1,6 @@
 import numpy as np
 import random
 from agent  import agent
-#from BlackjackSM import BlackjackSM
-#import pdb
 
 import torch
 import torch.nn as nn
@@ -13,23 +11,6 @@ from torch.autograd import Variable
 from dealer import dealer
 import time 
 
-
-# NN in DQN algorithm takes a state as an input
-# and produces Q values for all four actions
-# stand / hit / split / doubling 
-
-
-import numpy as np
-import random
-#from BlackjackSM import BlackjackSM
-import pdb
-
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-from torch.autograd import Variable
-import time
 
 FloatTensor = torch.FloatTensor
 LongTensor = torch.LongTensor
@@ -112,28 +93,6 @@ class DQN(nn.Module):
 			x = layer(x)
 		return x
 
-# initialize model
-#modell = DQN(modules)
-#print(modell)
-#time.sleep(10)
-
-#try: 
-#	model.load_state_dict(torch.load("models/blackjack_DQN_" + str(NUM_LAYERS) + "-" + str(k) + ".pt"))
-#	print("loaded saved model")
-#except:
-#	print("no saved model")
-
-
-
-# optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=WEIGHT_DECAY)
-# optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9, weight_decay=WEIGHT_DECAY)
-# memory = Memory(20000)
-# model.eval()
-
-# print("check")
-# time.sleep(100)
-
-
 
 class DQNAgent(agent):
 
@@ -147,39 +106,7 @@ class DQNAgent(agent):
 		self.optimizer = optim.SGD(self.model.parameters(), lr=learning_rate, momentum=0.9, weight_decay=WEIGHT_DECAY)
 		self.training = True 
 
-		# basic NN, next we will perform optimization of its parameters 
-		'''
-		self.model = (torch.nn.Sequential(
-			torch.nn.Linear(4, 8), # first layer takes 5 values: 4 for the state and 1 for the action 
-			torch.nn.ReLU(),
-			torch.nn.Linear(8, 16),
-			torch.nn.ReLU(),
-			torch.nn.Linear(16, 32),
-			torch.nn.ReLU(),
-			torch.nn.Linear(32, 16),
-			torch.nn.ReLU(),
-			torch.nn.Linear(16, 1),
-		))
-		'''
-		#self.model = (torch.nn.Sequential(
-		#	torch.nn.Linear(4, 52), # first layer takes 5 values: 4 for the state and 1 for the action 
-		#	torch.nn.ReLU(),
-		#	torch.nn.Linear(52, 38),
-		#	torch.nn.ReLU(),
-		#	torch.nn.Linear(27, 20),
-		#	torch.nn.ReLU(),
-		#	torch.nn.Linear(14, 10),
-		#	torch.nn.ReLU(),
-		#	torch.nn.Linear(10, 7),
-		#	torch.nn.ReLU(),
-		#	torch.nn.Linear(7, 5),
-		#	torch.nn.ReLU(),
-		#	torch.nn.Linear(5, 4),
-		#	torch.nn.ReLU(),
-		#	torch.nn.Linear(4, 3),
-		#	torch.nn.ReLU(),
-		#	torch.nn.Linear(3, 2),
-		#))
+		
 
 	def state_approx(self, hand): # approximates the state as a tuple of 4 elements 
 		#if action == 's':
@@ -314,180 +241,5 @@ class DQNAgent(agent):
 
 			self.counter += 1
 
-
-		# TODO: implement the policy method (epsilon greedy)
 		# TODO: initialization of the NN? 
-
-
-
-
-
-
-
-			
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-			#print('start')
-			#print(current_agent_hand)
-			#print(next_agent_hand)
-
-			#if agent_hands:
-			#	Q_true = self.gamma * self.Q_max(next_state) # this is label if next state is not the terminal 
-			#	Q_true = Q_true.detach()
-			#else:
-			#	Q_true = reward # this is label if the next state is the terminal   
-
-			#action = actions.pop(0)
-
-			#print(Q_true)
-			#print(action)
-			#time.sleep(10)
-			#self.DQN_update(Q_true, current_state, action) # updates NN  
-
-
-
-    			         
-
-		        
-   
-                  
-#agent = DQNAgent()
-#print(agent.model)
-#hand = [['2','3'],'K']
-#test = agent.policy(hand)
-#test = agent.policy(hand)
-#print(test)
-
-
-
-# training helper functions
-
-'''
-counter = 0
-def select_action():
-	global counter
-	unif_draw = np.random.rand()
-	if counter < start_period:
-		return LongTensor(np.array([random.choice(state_machine.actions())]))
-
-	eps = EPS_END + max((EPS_START - EPS_END) * (1 - np.exp((counter - start_period - decay_period)/EPS_DECAY)), 0)
-	
-	scores = model(Variable(FloatTensor(np.array([state_machine.state()])), volatile=True)).data
-	mask = ByteTensor(1 - state_machine.mask())
-	best_action = (scores.masked_fill_(mask, -16)).max(-1)[1]
-
-	if unif_draw > eps:
-		return LongTensor(best_action)
-	else:
-		actions = state_machine.actions()
-		actions.remove(best_action[0])
-		return LongTensor(np.array([random.choice(actions)]))
-
-		# actions = state_machine.actions()
-		# p = np.abs(scores.numpy()[0])
-		# p = (max(np.max(p),1) - p)[actions]
-		# p = p / np.sum(p)
-		# choice = np.array([np.random.choice(actions, p = p)])
-		# return LongTensor(choice)
-
-def optimize_model():
-	if len(memory) < BATCH_SIZE:
-		return	
-	model.train()
-	sample = memory.sample(BATCH_SIZE)
-
-	batch = list(zip(*sample))
-	state_batch = Variable(torch.cat(batch[0]))
-	action_batch = Variable(torch.cat(batch[1]))
-	reward_batch = Variable(torch.cat(batch[3]))
-
-	Q_sa = model(state_batch).gather(1, action_batch.view(-1,1)).squeeze()
-	
-	V_s = Variable(torch.zeros(BATCH_SIZE).type(FloatTensor))
-	not_terminal = ByteTensor(tuple(map(lambda s: s is not None, batch[2])))
-	if not_terminal.sum() > 0: 
-		model.eval()
-		not_terminal_states = Variable(torch.cat([s for s in batch[2] if s is not None]), volatile=True)
-		masks = ByteTensor(np.array([1 - state_machine.mask_for(s) for s in not_terminal_states.data.numpy()]))
-		V_s[not_terminal] = (model(not_terminal_states).data.masked_fill_(masks, -16)).max(1)[0]
-		model.train()
-	observed_sa = reward_batch + (V_s * GAMMA)
-
-	loss = F.smooth_l1_loss(Q_sa, observed_sa)
-
-	# pdb.set_trace()
-
-	optimizer.zero_grad()
-	loss.backward()
-	nn.utils.clip_grad_norm(model.parameters(), max_norm=2, norm_type=2)
-	optimizer.step()
-	model.eval()
-
-# training
-iterations = []
-avg_reward_graph = []
-avg_reward = 0
-avg_score = 0
-for episode in range(num_episodes):
-	state_machine.new_hand()
-	state = FloatTensor([state_machine.state()])
-
-	while True:
-		action = select_action()
-		state_machine.do(int(action[0]))
-		reward = FloatTensor([state_machine.reward()])
-
-		if not state_machine.terminal:
-			next_state = FloatTensor([state_machine.state()])
-		else:
-			next_state = None
-
-		memory.push([state, action, next_state, reward])
-		state = next_state
-
-		if counter % C == 0:
-			optimize_model()
-		counter += 1
-
-		if state_machine.terminal:
-			break
-
-	if episode > (num_episodes - 5000):
-		avg_score += state_machine.reward() / 5000
-
-	avg_reward += state_machine.reward()
-	if episode % graph_interval == 0:
-		iterations.append(episode)
-		avg_reward_graph.append(avg_reward / graph_interval)
-		avg_reward = 0
-
-
-torch.save(model.state_dict(), "models/blackjack_DQN_" + str(NUM_LAYERS) + "-" + str(k) + ".pt")
-print("saved model")
-
-print(avg_score)
-
-import matplotlib.pyplot as plt
-plt.title("Average Reward Over Time for a DQN with " + str(NUM_LAYERS) + " Layers and Size Factor " + str(k))
-plt.ylabel("return per episode")
-plt.xlabel("number of episodes")
-plt.plot(iterations, avg_reward_graph)
-plt.show()
-'''
 
